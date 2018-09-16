d740a088b6a97552de2e885ce3ccd92f969853f1
BoredTweak <alex@coderpages.com>
2018-09-16 10:39:38 -0700

    Minor credit and link to newsapi

--- news.py
+++ news.py
@@ -1,3 +1,4 @@
+# extends NewsAPI from newsapi.org
 from newsapi import NewsApiClient
 with open('credentials.config', 'r') as config:
     credentials=config.read().replace('\n','')
@@ -6,8 +7,6 @@ newsapi = NewsApiClient(api_key = credentials)
 top_headlines = newsapi.get_top_headlines(country='us')
 
 articles = top_headlines['articles']
-
-#print articles[0]
    
 taglines = [ (o['title'], o['source']['name']) for o in articles]
 print taglines
